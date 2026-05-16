552b61a47b5665145fd5ddc7a16e58eef42b2a62
Kush <kushagra2004s@gmail.com>
2026-05-15 17:36:43 -0700

    Add flake entrypoint

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Kush's NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+}
